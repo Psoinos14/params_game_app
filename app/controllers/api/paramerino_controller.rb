94317a0b77d43c2798e4eb29f6a60126d4b6a23c
shane <smp1993@gmail.com>
2019-06-26 21:32:47 -0400

    added number guessing game to app using segment_url

--- app/controllers/api/paramerino_controller.rb
+++ app/controllers/api/paramerino_controller.rb
@@ -3,16 +3,35 @@ class Api::ParamerinoController < ApplicationController
   def query
     @name = params["name"].upcase
     if @name.chars[0] == "A"
-      puts "Hey, your name starts with the first letter of the alphabet!"
+      @name = "Hey, your name starts with the first letter of the alphabet!"
+    else
+      @name = "Your name is #{@name}"
+    end
+    @number = 36
+    if params["guess"].to_i < @number
+      @guess = "Too low."
+    elsif params["guess"].to_i > @number
+      @guess = "Too high."
+    else
+      @guess = "That's it!"  
     end
     render 'query_paramerino.json.jb'
+  end
+
+  def url_segment_method
+    @name = params["name"]
+    render 'segment_param.json.jb'
+  end
+  def url_segment_number_guess
     @number = 36
-    if params["number"].to_i < 36
-      puts "Too low."
-    elsif params["number"].to_i > 36
-      puts "Too high."
+    @guess = params["guess"]
+    if params["guess"].to_i < @number
+      @guess = "Too low."
+    elsif params["guess"].to_i > @number
+      @guess = "Too high."
     else
-      puts "That's it!"  
+      @guess = "That's it!"  
     end
+    render 'segment_url_number_guess.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/pillow' => 'paramerino#query'
+    get 'segment_url/:name' => 'paramerino#url_segment_method'
+    get 'segment_url_2/:guess' => 'paramerino#url_segment_number_guess'
   end
 end
